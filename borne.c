00593a2669a1fb1734a96d919334c4fa8d07b082
etienne <etienne@lebarillier.fr>
2017-12-21 12:54:40 +0100

    test client

--- borne.c
+++ borne.c
@@ -11,12 +11,29 @@ int main (int argc, char *argv[]) {
 	int connectionInfo; 
 	struct sockaddr_in serveur;
 	serveur.sin_family = AF_INET;
-	serveur.sin_port = htons(7979);	
-	serveur.sin_addr.s_addr = inet_addr("192.168.0.102");
+	serveur.sin_port = htons(7777);	
+	serveur.sin_addr.s_addr = inet_addr("10.100.13.22");
 	int s = socket(AF_INET,SOCK_STREAM,0);
-	char plaqueImmatriculation[] = "AB333EA\0";
-	char categorie[] = "A\0";
-	int duree = 20;
+	char plaqueImmatriculation[7];
+	char categorie = 'A';
+	int duree;
+
+
+	printf("catégorie : ");
+	scanf("%c", &categorie);
+	printf("plaque d'immatriculation : ");
+	scanf("%s", plaqueImmatriculation);	
+	printf("duree : ");
+	scanf("%d", &duree);
+
+
+
+
+	printf("%s \n", plaqueImmatriculation);
+	printf("%c \n", categorie);
+	printf("%d \n", duree);
+
+
 	if (s < 0) {
 		perror("Erreur de socket");
 	} else {
@@ -24,8 +41,9 @@ int main (int argc, char *argv[]) {
 		if (connect(s,(struct sockaddr*) &serveur,sizeof(serveur)) < 0) {
 			perror("Erreur de connexion");
 		}else {
+			printf("Salut");
 			write(s,plaqueImmatriculation,8);
-			write(s,categorie,2);
+			write(s,categorie,1);
 			write(s,&duree,sizeof(int));
 			printf("Message envoyé");
 		}
